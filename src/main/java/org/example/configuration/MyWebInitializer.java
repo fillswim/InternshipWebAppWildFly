9ecbf9c0a2ce87ae0d1f326353bf06489a0661e1
Oleg Filatov <fillswim@gmail.com>
2022-07-04 07:05:23 +0300

    Update MyWebInitializer

--- src/main/java/org/example/configuration/MyWebInitializer.java
+++ src/main/java/org/example/configuration/MyWebInitializer.java
@@ -1,20 +1,10 @@
 package org.example.configuration;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 // Создание класса для реализации Dispatcher Servlet
 public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    // Не используется
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
@@ -30,23 +20,3 @@ public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletI
         return new String[]{"/"};
     }
 }
-
-//public class MyWebInitializer implements WebApplicationInitializer {
-//
-//    @Override
-//    public void onStartup(final ServletContext servletContext) throws ServletException {
-//
-//        AnnotationConfigWebApplicationContext context =
-//                new AnnotationConfigWebApplicationContext();
-//
-//        context.scan("org.example");
-//        servletContext.addListener(new ContextLoaderListener(context));
-//
-//        ServletRegistration.Dynamic appServlet =
-//                servletContext.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
-//        appServlet.setLoadOnStartup(1);
-//        appServlet.addMapping("/");
-//    }
-//
-//
-//}
