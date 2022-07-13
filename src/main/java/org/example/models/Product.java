e168a9a4ff91bf70ae328357ac17096f3ce231ef
Oleg Filatov <fillswim@gmail.com>
2022-07-13 21:42:06 +0300

    Add Product.java

---
+++ src/main/java/org/example/models/Product.java
@@ -0,0 +1,26 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private Double price;
+
+}
