8fdfbd1b9b8b83b6c47419842a491b44843d214f
Oleg Filatov <fillswim@gmail.com>
2022-07-13 21:42:33 +0300

    Add BucketDetails.java

---
+++ src/main/java/org/example/models/BucketDetails.java
@@ -0,0 +1,28 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bucket_details")
+public class BucketDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int amount;
+
+}
