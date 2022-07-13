6c84971107c1488a976a74e9aa37348bf7cd513f
Oleg Filatov <fillswim@gmail.com>
2022-07-13 21:48:17 +0300

    Update Info.java

--- src/main/java/org/example/models/Info.java
+++ src/main/java/org/example/models/Info.java
@@ -1,7 +1,16 @@
 package org.example.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "information")
 public class Info {
@@ -19,44 +28,4 @@ public class Info {
     @Column(name = "copyright")
     private String copyright;
 
-    public Info() {
-    }
-
-    public Info(String name, String period, String copyright) {
-        this.name = name;
-        this.period = period;
-        this.copyright = copyright;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(String period) {
-        this.period = period;
-    }
-
-    public String getCopyright() {
-        return copyright;
-    }
-
-    public void setCopyright(String copyright) {
-        this.copyright = copyright;
-    }
 }
