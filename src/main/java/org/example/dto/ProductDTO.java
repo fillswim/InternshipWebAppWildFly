c0c8ca7690197823d2bbe520db5097b61947ec4c
Oleg Filatov <fillswim@gmail.com>
2022-07-14 15:03:30 +0300

    Add ProductDTO

---
+++ src/main/java/org/example/dto/ProductDTO.java
@@ -0,0 +1,20 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDTO {
+
+    private int id;
+
+    private String title;
+
+    private Double price;
+
+}
