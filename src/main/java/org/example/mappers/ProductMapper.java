7029ccb7a3173c75c79eced5436419e7201d3750
Oleg Filatov <fillswim@gmail.com>
2022-07-14 16:41:47 +0300

    Add ProductMapper

---
+++ src/main/java/org/example/mappers/ProductMapper.java
@@ -0,0 +1,28 @@
+package org.example.mappers;
+
+import org.example.dto.ProductDTO;
+import org.example.models.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+
+    public ProductDTO mapToProductDto(Product product) {
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .title(product.getTitle())
+                .price(product.getPrice())
+                .build();
+    }
+
+    public Product mapToProduct(ProductDTO productDTO) {
+
+        return Product.builder()
+                .id(productDTO.getId())
+                .title(productDTO.getTitle())
+                .price(productDTO.getPrice())
+                .build();
+    }
+
+}
