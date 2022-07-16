72693c830e9974760001b211068af5599ba011d4
Oleg Filatov <fillswim@gmail.com>
2022-07-16 11:26:17 +0300

    Add ManufacturerDTO

---
+++ src/main/java/org/example/dto/ManufacturerDTO.java
@@ -0,0 +1,20 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ManufacturerDTO {
+
+    private int id;
+
+    private String title;
+
+    private String address;
+
+}
