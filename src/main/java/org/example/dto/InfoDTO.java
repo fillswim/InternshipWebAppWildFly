0cb0f42691b31e769b4c66c3cb62ba12c9366af1
Oleg Filatov <fillswim@gmail.com>
2022-07-16 11:25:24 +0300

    Add InfoDTO

---
+++ src/main/java/org/example/dto/InfoDTO.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InfoDTO {
+
+    private String header;
+
+    private String footer;
+
+
+}
