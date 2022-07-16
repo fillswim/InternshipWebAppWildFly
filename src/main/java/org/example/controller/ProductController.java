089a93510c45a7163623c653da4f9834c83d37c1
Oleg Filatov <fillswim@gmail.com>
2022-07-16 13:53:56 +0300

    Update ProductController

--- src/main/java/org/example/controller/ProductController.java
+++ src/main/java/org/example/controller/ProductController.java
@@ -80,6 +80,9 @@ public class ProductController {
         ProductDTO productDTO = new ProductDTO();
         model.addAttribute("product", productDTO);
 
+        List<ManufacturerDTO> manufacturerDTOS = manufacturerService.getAllManufacturerDTOS();
+        model.addAttribute("manufacturers", manufacturerDTOS);
+
         InfoDTO infoDTO = infoService.getInfoDTOBuId(0);
         model.addAttribute("infoDTO", infoDTO);
 
@@ -90,24 +93,41 @@ public class ProductController {
     @PostMapping("/saveProduct")
     public String saveProduct(@ModelAttribute("product") ProductDTO productDTO) {
 
-        Product product = productService.getProductById(productDTO.getId());
+        int productDTOId = productDTO.getId();
 
-        if ((productDTO.getTitle() != null) && (!productDTO.getTitle().equals(product.getTitle()))) {
-            product.setTitle(productDTO.getTitle());
+        if (productDTOId != 0) {
+            Product product = productService.getProductById(productDTO.getId());
+
+            if ((productDTO.getTitle() != null) && (!productDTO.getTitle().equals(product.getTitle()))) {
+                product.setTitle(productDTO.getTitle());
+            }
+
+            if ((productDTO.getPrice() != null) && (productDTO.getPrice() != product.getPrice())) {
+                product.setPrice(productDTO.getPrice());
+            }
+
+            if ((productDTO.getManufacturerId() != 0)
+                    && (productDTO.getManufacturerId() != product.getManufacturer().getId())) {
+
+                Manufacturer manufacturer = manufacturerService.getManufacturerById(productDTO.getManufacturerId());
+                product.setManufacturer(manufacturer);
+            }
+
+            productService.saveProduct(product);
+        } else {
+
+            int manufacturerId = productDTO.getManufacturerId();
+            Manufacturer manufacturer = manufacturerService.getManufacturerById(manufacturerId);
+
+            Product product = Product.builder()
+                    .title(productDTO.getTitle())
+                    .price(productDTO.getPrice())
+                    .manufacturer(manufacturer)
+                    .build();
+
+            productService.saveProduct(product);
+
         }
-
-        if ((productDTO.getPrice() != null) && (productDTO.getPrice() != product.getPrice())) {
-            product.setPrice(productDTO.getPrice());
-        }
-
-        if ((productDTO.getManufacturerId() != 0)
-                && (productDTO.getManufacturerId() != product.getManufacturer().getId())) {
-
-            Manufacturer manufacturer = manufacturerService.getManufacturerById(productDTO.getManufacturerId());
-            product.setManufacturer(manufacturer);
-        }
-
-        productService.saveProduct(product);
 
         return "redirect:/edit";
     }
