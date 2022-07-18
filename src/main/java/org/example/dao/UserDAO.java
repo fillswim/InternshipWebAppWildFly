e883c4c5ccdcb576d362209c40451fe317171b26
Oleg Filatov <fillswim@gmail.com>
2022-07-18 14:16:12 +0300

    Add UserDAO

---
+++ src/main/java/org/example/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package org.example.dao;
+
+import org.example.models.User;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    List<User> showAllUsers();
+
+    User loadUserByUsername(String username);
+
+}
---
+++ src/main/java/org/example/dao/UserDAOImpl.java
@@ -0,0 +1,56 @@
+package org.example.dao;
+
+import org.example.models.Product;
+import org.example.models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    private final SessionFactory sessionFactory;
+
+    public UserDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<User> showAllUsers() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root);
+
+        Query<User> query = session.createQuery(criteriaQuery);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public User loadUserByUsername(String username) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("username"), username));
+
+        Query<User> query = session.createQuery(criteriaQuery);
+
+        return query.getSingleResult();
+    }
+
+}
