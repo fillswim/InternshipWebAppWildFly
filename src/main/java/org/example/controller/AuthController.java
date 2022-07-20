43eab3763c66b0d9841846097bc17057be1cdc0e
Oleg Filatov <fillswim@gmail.com>
2022-07-20 09:34:43 +0300

    Update AuthController

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
+import org.example.dto.InfoDTO;
 import org.example.dto.UserDTO;
+import org.example.service.InfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
+    private final InfoService infoService;
+
+    public AuthController(InfoService infoService) {
+        this.infoService = infoService;
+    }
+
     @GetMapping("/login")
     public String showLoginPage(Model model) {
 
         model.addAttribute("loginUser", new UserDTO());
+
+        InfoDTO infoDTO = infoService.getInfoDTOBuId(0);
+        model.addAttribute("infoDTO", infoDTO);
 
         return "login";
     }
