5f66102ff21d13484701452ba04838e8545db7b3
Oleg Filatov <fillswim@gmail.com>
2022-07-20 11:46:36 +0300

    Update AuthController

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.example.service.InfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
@@ -17,7 +18,17 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String showLoginPage(Model model) {
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout,
+                                Model model) {
+
+        if (error != null) {
+            model.addAttribute("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addAttribute("msg", "You've been logged out successfully.");
+        }
 
         model.addAttribute("loginUser", new UserDTO());
 
