ddc6dd061d7fb0882f2e499f855768368b46b7ba
Oleg Filatov <fillswim@gmail.com>
2022-07-21 16:26:15 +0300

    Update BucketDetailsDAO

--- src/main/java/org/example/dao/BucketDetailsDAO.java
+++ src/main/java/org/example/dao/BucketDetailsDAO.java
@@ -1,20 +1,21 @@
 package org.example.dao;
 
+import org.example.models.Bucket;
 import org.example.models.BucketDetails;
 import org.example.models.Product;
+import org.example.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BucketDetailsDAO {
 
-    List<BucketDetails> findAllBucketDetails();
-
-    BucketDetails getBucketDetailsByProduct(int productId);
-
-    BucketDetails getBucketDetailsByProduct(Product product);
-
     void saveBucketDetails(BucketDetails bucketDetails);
 
-    void deleteBucketDetailsById(int bucketDetailsId);
+    void deleteBucketDetails(BucketDetails bucketDetails);
+
+    Optional<BucketDetails> findBucketDetailsByBucketAndProduct(Bucket bucket, Product product);
+
+    List<BucketDetails> findBucketDetailsByBucket(Bucket bucket);
 
 }
--- src/main/java/org/example/dao/BucketDetailsDAOImpl.java
+++ src/main/java/org/example/dao/BucketDetailsDAOImpl.java
@@ -1,17 +1,17 @@
 package org.example.dao;
 
+import org.example.models.Bucket;
 import org.example.models.BucketDetails;
 import org.example.models.Product;
+import org.example.models.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class BucketDetailsDAOImpl implements BucketDetailsDAO {
@@ -23,70 +23,6 @@ public class BucketDetailsDAOImpl implements BucketDetailsDAO {
     }
 
     @Override
-    public List<BucketDetails> findAllBucketDetails() {
-
-        Session session = sessionFactory.getCurrentSession();
-
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<BucketDetails> criteriaQuery = criteriaBuilder.createQuery(BucketDetails.class);
-
-        Root<BucketDetails> root = criteriaQuery.from(BucketDetails.class);
-        criteriaQuery.select(root);
-
-        Query<BucketDetails> query = session.createQuery(criteriaQuery);
-
-        return query.getResultList();
-    }
-
-    @Override
-    public BucketDetails getBucketDetailsByProduct(int productId) {
-
-        Session session = sessionFactory.getCurrentSession();
-
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<BucketDetails> criteriaQuery = criteriaBuilder.createQuery(BucketDetails.class);
-
-        Root<BucketDetails> root = criteriaQuery.from(BucketDetails.class);
-        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("product").get("id"), productId));
-
-        Query<BucketDetails> query = session.createQuery(criteriaQuery);
-
-        BucketDetails bucketDetails;
-
-        try {
-            bucketDetails = query.getSingleResult();
-        } catch (Exception e) {
-            bucketDetails = null;
-        }
-
-        return bucketDetails;
-    }
-
-    @Override
-    public BucketDetails getBucketDetailsByProduct(Product product) {
-
-        Session session = sessionFactory.getCurrentSession();
-
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<BucketDetails> criteriaQuery = criteriaBuilder.createQuery(BucketDetails.class);
-
-        Root<BucketDetails> root = criteriaQuery.from(BucketDetails.class);
-        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("product"), product));
-
-        Query<BucketDetails> query = session.createQuery(criteriaQuery);
-
-        BucketDetails bucketDetails;
-
-        try {
-            bucketDetails = query.getSingleResult();
-        } catch (Exception e) {
-            bucketDetails = null;
-        }
-
-        return bucketDetails;
-    }
-
-    @Override
     public void saveBucketDetails(BucketDetails bucketDetails) {
 
         Session session = sessionFactory.getCurrentSession();
@@ -95,17 +31,56 @@ public class BucketDetailsDAOImpl implements BucketDetailsDAO {
     }
 
     @Override
-    public void deleteBucketDetailsById(int bucketDetailsId) {
+    public void deleteBucketDetails(BucketDetails bucketDetails) {
+
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(bucketDetails);
+
+    }
+
+    @Override
+    public Optional<BucketDetails> findBucketDetailsByBucketAndProduct(Bucket bucket, Product product) {
 
         Session session = sessionFactory.getCurrentSession();
 
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaDelete<BucketDetails> criteriaDelete = criteriaBuilder.createCriteriaDelete(BucketDetails.class);
+        CriteriaQuery<BucketDetails> criteriaQuery = criteriaBuilder.createQuery(BucketDetails.class);
 
-        Root<BucketDetails> root = criteriaDelete.from(BucketDetails.class);
-        criteriaDelete.where(criteriaBuilder.equal(root.get("id"), bucketDetailsId));
+        Root<BucketDetails> root = criteriaQuery.from(BucketDetails.class);
+        Predicate predicateForBucket = criteriaBuilder.equal(root.get("bucket"), bucket);
+        Predicate predicateForProduct = criteriaBuilder.equal(root.get("product"), product);
 
-        session.createQuery(criteriaDelete).executeUpdate();
+        Predicate finalPredicate = criteriaBuilder.and(predicateForBucket, predicateForProduct);
+
+        criteriaQuery.where(finalPredicate);
+
+        Query<BucketDetails> query = session.createQuery(criteriaQuery);
+
+        BucketDetails bucketDetails;
+
+        try {
+            bucketDetails = query.getSingleResult();
+        } catch (Exception e) {
+            bucketDetails = null;
+        }
+
+        return Optional.ofNullable(bucketDetails);
+    }
+
+    @Override
+    public List<BucketDetails> findBucketDetailsByBucket(Bucket bucket) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<BucketDetails> criteriaQuery = criteriaBuilder.createQuery(BucketDetails.class);
+
+        Root<BucketDetails> root = criteriaQuery.from(BucketDetails.class);
+        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("bucket"), bucket));
+
+        Query<BucketDetails> query = session.createQuery(criteriaQuery);
+
+        return query.getResultList();
 
     }
 }
