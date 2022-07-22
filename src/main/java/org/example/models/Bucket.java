f00231fca574ad81cba98fbc5a7302348171d06e
Oleg Filatov <fillswim@gmail.com>
2022-07-22 16:22:42 +0300

    Update Bucket

--- src/main/java/org/example/models/Bucket.java
+++ src/main/java/org/example/models/Bucket.java
@@ -2,8 +2,11 @@ package org.example.models;
 
 import lombok.*;
 import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -19,6 +22,12 @@ public class Bucket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @UpdateTimestamp
+    private LocalDateTime updated;
 
     @Enumerated(EnumType.STRING)
     private BucketStatus bucketStatus;
