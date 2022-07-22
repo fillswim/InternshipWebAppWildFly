046481261df8353599368669a31d13e72c25bad6
Oleg Filatov <fillswim@gmail.com>
2022-07-22 16:18:49 +0300

    Add OrderDTO

---
+++ src/main/java/org/example/dto/OrderDTO.java
@@ -0,0 +1,24 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderDTO {
+
+    private int id;
+
+    private LocalDateTime date;
+
+    private String status;
+
+    private double sum;
+
+}
