ed765f30abe23b6cf2ac01c2e0701de272eecaae
Oleg Filatov <fillswim@gmail.com>
2022-07-25 16:01:50 +0300

    Add Order

---
+++ src/main/java/org/example/models/Order.java
@@ -0,0 +1,41 @@
+package org.example.models;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "bucket_id")
+    private Bucket bucket;
+
+
+}
